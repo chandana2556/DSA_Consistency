ca1b44a568b7d1287aaecfb1b9ac262c78345252
chandana2556 <madugulachandana2625@gmail.com>
2025-10-20 23:15:18 +0530

    Added 283 Move Zeroes

---
+++ Arrays/Leetcode/283_MoveZeroes.cpp
@@ -0,0 +1,18 @@
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if (nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        for(int i=j;i<nums.size();i++){
+            nums[i]=0;
+        }
+    }
+};
