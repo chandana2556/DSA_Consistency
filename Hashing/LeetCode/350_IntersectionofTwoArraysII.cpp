f442de604180f9042b61eb64bf95ee7612406ae8
chandana <madugulachandana2625@gmail.com>
2025-11-03 23:13:52 +0530

    Added Leetcode 350 - Intersection of Two Arrays II solution

---
+++ Hashing/LeetCode/350_IntersectionofTwoArraysII.cpp
@@ -0,0 +1,21 @@
+Intersection of Two Arrays II
+TC:O(n+m)
+SC:O(n)
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>freq;
+        vector<int>result;
+        for(int num : nums1){
+            freq[num]++;
+        }
+        for(int num : nums2){
+            if(freq[num]>0){
+                result.push_back(num);
+                freq[num]--;
+            }
+        }
+        return result;
+    }
+};
